0bd35e1fcc5f2e8161484f35b38168f9a48c0242
gregorytomilin <gregory.tomilin@gmail.com>
2020-11-01 01:48:10 +0300

    Первый этап skills

--- src/scripts/skills.js
+++ src/scripts/skills.js
@@ -1,47 +1,47 @@
 import Vue from "vue";
-import axios from "axios";
+// import axios from "axios";
 
-const baseUrl = process.env.BASE_URL;
+// const baseUrl = process.env.BASE_URL;
 
-axios.defaults.baseURL = baseUrl;
+// axios.defaults.baseURL = baseUrl;
 
-const skill = {
-  template: "#skill",
-  props: {
-    skillName: String,
-    skillPercents: Number
-  },
-  methods: {
-    drawCircle() {
-      const circle = this.$refs["color-circle"];
-      const dashOffset = parseInt(
-        getComputedStyle(circle).getPropertyValue("stroke-dasharray")
-      );
-      const percent = (dashOffset / 100) * (100 - this.skillPercents);
+const skillsItem = {
+  template: "#skills-item",
+  // props: {
+  //   skillName: String,
+  //   skillPercents: Number
+  // },
+  // methods: {
+  //   drawCircle() {
+  //     const circle = this.$refs["color-circle"];
+  //     const dashOffset = parseInt(
+  //       getComputedStyle(circle).getPropertyValue("stroke-dasharray")
+  //     );
+  //     const percent = (dashOffset / 100) * (100 - this.skillPercents);
 
-      circle.style.strokeDashoffset = percent;
-    }
-  },
-  mounted() {
-    this.drawCircle();
-  }
+  //     circle.style.strokeDashoffset = percent;
+  //   }
+  // },
+  // mounted() {
+  //   this.drawCircle();
+  // }
 };
 
 const skillsRow = {
   template: "#skills-row",
   components: {
-    skill
+    skillsItem,
   },
-  props: {
-    skills: Array,
-    category: Object
-  },
-  computed: {
-    categorySkills() {
-      const matchSkillAndCatId = skill => skill.category === this.category.id;
-      return this.skills.filter(matchSkillAndCatId);
-    }
-  }
+  // props: {
+  //   skills: Array,
+  //   category: Object
+  // },
+  // computed: {
+  //   categorySkills() {
+  //     const matchSkillAndCatId = skill => skill.category === this.category.id;
+  //     return this.skills.filter(matchSkillAndCatId);
+  //   }
+  // }
 };
 
 new Vue({
@@ -49,23 +49,34 @@ new Vue({
   components: {
     skillsRow
   },
-  data: {
-    skills: [],
-    categories: []
-  },
-  methods: {
-    async fetchCategories() {
-      const { data: categories } = await axios.get("/categories/1");
-      this.categories = categories;
-    },
-    async fetchSkills() {
-      const { data: skills } = await axios.get("/skills/1");
-      this.skills = skills;
+  data(){
+    return {
+      skills: []
     }
   },
-  async created() {
-    await this.fetchCategories();
-    await this.fetchSkills();
+  created(){
+    this.skills = require("../data/skills.json")
   },
+  mounted(){
+    
+  },
+  // data: {
+  //   skills: [],
+  //   categories: []
+  // },
+  // methods: {
+  //   async fetchCategories() {
+  //     const { data: categories } = await axios.get("/categories/1");
+  //     this.categories = categories;
+  //   },
+  //   async fetchSkills() {
+  //     const { data: skills } = await axios.get("/skills/1");
+  //     this.skills = skills;
+  //   }
+  // },
+  // async created() {
+  //   await this.fetchCategories();
+  //   await this.fetchSkills();
+  // },
   template: "#skills-list"
 });
