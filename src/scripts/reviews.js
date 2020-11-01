c9c535cb68a7ead5318ff93ad7062e059e692c3a
gregorytomilin <gregory.tomilin@gmail.com>
2020-11-01 15:17:20 +0300

    Завершение works + reviews

--- src/scripts/reviews.js
+++ src/scripts/reviews.js
@@ -1,87 +1,129 @@
 import Vue from "vue";
-import axios from "axios";
+import { Swiper, SwiperSlide, directive } from 'vue-awesome-swiper';
+import 'swiper/swiper-bundle.css'
 
-const baseUrl = process.env.BASE_URL;
+// import axios from "axios";
 
-axios.defaults.baseURL = baseUrl;
+// const baseUrl = process.env.BASE_URL;
 
-new Vue({
+// axios.defaults.baseURL = baseUrl;
+new Vue ({
   el: "#reviews-component",
   template: "#reviews",
-  data() {
-    return {
-      reviews: [],
-      strafe: 0
-    };
+  components: {
+    Swiper, SwiperSlide
   },
-  methods: {
-    arrWithRequiredImages(array) {
-      return array.map(item => {
-        const requredPic = require(`../images/content/${item["author-pic"]}`);
-        item["author-pic"] = requredPic;
-
-        return item;
-      });
-    },
-    slide(direction) {
-      const slider = this.$refs["reviews-slider"];
-      const elemWidth = +slider.getBoundingClientRect().width;
-      const oneItemWidth = +slider.firstElementChild.getBoundingClientRect()
-        .width;
-      const itemsInView = 2;
-      const availableWidth =
-        oneItemWidth * (slider.children.length - itemsInView);
-
-      switch (direction) {
-        case "next":
-          if (Math.abs(this.strafe) <= availableWidth) {
-            this.strafe += elemWidth;
-          }
-          break;
-        case "prev":
-          if (Math.abs(this.strafe) > 0) {
-            this.strafe -= elemWidth;
-          }
-          break;
+  data(){
+    return {
+      reviews : [],
+      sliderOption: {
+        slidesPerView: 2
       }
-
-      slider.style.transform = `translateX(-${this.strafe}px)`;
-    },
-    resetSliderOnResize() {
-      const throttledSliderReset = this.debounse(() => {
-        const slider = this.$refs["reviews-slider"];
-        this.strafe = 0;
-        slider.style.transform = "translateX(0)";
-      }, 1000);
-
-      window.addEventListener("resize", throttledSliderReset);
-    },
-    debounse(fn, ms) {
-      let timer = null;
-
-      return function(...args) {
-        const onComplete = () => {
-          fn.apply(this, args);
-          timer = null;
-        };
-
-        if (timer) {
-          clearTimeout(timer);
-        }
-
-        timer = setTimeout(onComplete, ms);
-      };
-    },
-    async fetchReviews() {
-      const { data: reviews } = await axios.get("/reviews/1");
-
-      this.reviews = reviews;
     }
   },
-  async created() {
-    await this.fetchReviews();
+  methods: {
+    requireImagesToArray(data) {
+      return data.map((item) => {
+        const requireImage = require(`../images/content/${item.pic}`).default;
+        item.photo = requireImage;
+        return item;
+      })
+    },
+    slide(direction){
+      const slider = this.$refs["slider"].$swiper;
+      switch(direction) {
+        case "next" :
+          slider.slideNext();
+        break;
+        case "prev" :
+          slider.slidePrev();
+        break;
+      }
+    }
   },
-  mounted() {
-    this.resetSliderOnResize();
+  created() {
+    const data = require("../data/reviews.json");
+    this.reviews = this.requireImagesToArray(data);
   }
-});
+})
+
+// new Vue({
+//   el: "#reviews-component",
+//   template: "#reviews",
+//   data() {
+//     return {
+//       reviews: [],
+//       strafe: 0
+//     };
+//   },
+//   methods: {
+//     arrWithRequiredImages(array) {
+//       return array.map(item => {
+//         const requredPic = require(`../images/content/${item["author-pic"]}`);
+//         item["author-pic"] = requredPic;
+
+//         return item;
+//       });
+//     },
+//     slide(direction) {
+//       const slider = this.$refs["reviews-slider"];
+//       const elemWidth = +slider.getBoundingClientRect().width;
+//       const oneItemWidth = +slider.firstElementChild.getBoundingClientRect()
+//         .width;
+//       const itemsInView = 2;
+//       const availableWidth =
+//         oneItemWidth * (slider.children.length - itemsInView);
+
+//       switch (direction) {
+//         case "next":
+//           if (Math.abs(this.strafe) <= availableWidth) {
+//             this.strafe += elemWidth;
+//           }
+//           break;
+//         case "prev":
+//           if (Math.abs(this.strafe) > 0) {
+//             this.strafe -= elemWidth;
+//           }
+//           break;
+//       }
+
+//       slider.style.transform = `translateX(-${this.strafe}px)`;
+//     },
+//     resetSliderOnResize() {
+//       const throttledSliderReset = this.debounse(() => {
+//         const slider = this.$refs["reviews-slider"];
+//         this.strafe = 0;
+//         slider.style.transform = "translateX(0)";
+//       }, 1000);
+
+//       window.addEventListener("resize", throttledSliderReset);
+//     },
+//     debounse(fn, ms) {
+//       let timer = null;
+
+//       return function(...args) {
+//         const onComplete = () => {
+//           fn.apply(this, args);
+//           timer = null;
+//         };
+
+//         if (timer) {
+//           clearTimeout(timer);
+//         }
+
+//         timer = setTimeout(onComplete, ms);
+//       };
+//     },
+//     async fetchReviews() {
+//       const { data: reviews } = await axios.get("/reviews/1");
+
+//       this.reviews = reviews;
+//     }
+//   },
+//   async created() {
+//     await this.fetchReviews();
+//   },
+//   mounted() {
+//     this.resetSliderOnResize();
+//   }
+// });
--- src/scripts/works.js
+++ src/scripts/works.js
@@ -13,24 +13,30 @@ const btns = {
  
 }
 const display = {
-  props: ["currentWork", "works"],
+  props: ["currentWork", "works", "currentIndex"],
   template: "#preview-display",
   components: {thumbs, btns},
   computed: {
     reversedWorks(){
       const works = [...this.works];
-      return works.reverse();
+      return works.slice(0, 3).reverse();
     }
   }
  
 }
 const tags = {
+  props: ["tags"],
   template: "#preview-tags"
 }
 const info = {
   props: ["currentWork"],
   template: "#preview-info",
-  components: {tags}
+  components: {tags},
+  computed: {
+    tagsArray(){
+      return this.currentWork.skills.split(',');
+    }
+  }
 
 }
 
@@ -46,10 +52,20 @@ new Vue({
   },
   computed:{
     currentWork(){
-      return this.works[this.currentIndex];
+      return this.works[0];
+    }
+  },
+  watch:{
+    currentIndex(value) {
+      this.mekeInfiniteLoopForIndex(value)
     }
   },
   methods: {
+    mekeInfiniteLoopForIndex(index){
+      const worksQuantity = this.works.length - 1;
+      if( index < 0 ) this.currentIndex = worksQuantity;
+      if ( index > worksQuantity) this.currentIndex = 0
+    },
     requireImagesToArray(data){
       return data.map(item => {
         const requiredImg = require(`../images/content/${item.photo}`).default;
@@ -59,11 +75,17 @@ new Vue({
     },
     slide(direction, e){
       this.$emit("slide", direction);
+      const lastItem = this.works[this.works.length - 1]
       switch(direction) {
         case "next" : 
+        this.works.push(this.works[0]);
+        this.works.shift();
         this.currentIndex++
         break;
         case "prev" : 
+        this.works.unshift(lastItem);
+        this.works.pop();
+
         this.currentIndex--
         break;
       }
