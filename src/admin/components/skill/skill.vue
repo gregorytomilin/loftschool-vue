413a1645f37c8d831cb4150e9308acae9ea1238e
Gregory Tomilin <gregory.tomilin@gmail.com>
2020-11-10 14:25:58 +0300

    Добавление проверки всех инпутов на пустоту

--- src/admin/components/skill/skill.vue
+++ src/admin/components/skill/skill.vue
@@ -10,7 +10,21 @@
 
   <div class="skill-component" v-else>
     <div class="title">
-      <app-input noSidePaddings v-model="currentSkill.title" />
+      <app-input noSidePaddings
+      v-model="currentSkill.title"
+      @input="checkForEmptyInputValue($event)"
+      @keydown.native.enter="onApprove"
+
+
+      ></app-input>
+
+      <tooltip
+              v-if="emptyInput.length > 0"
+              :errorText="errorText"
+              :text="emptyInput"
+              slot="tooltip"
+              v-model="emptyInput"
+      ></tooltip>
     </div>
     <div class="percent">
       <app-input v-model="currentSkill.percent" type="number" min="0" max="100" maxlength="3" />
@@ -34,11 +48,18 @@ export default {
       type: Object,
       default: () => {},
       required: true
-    }
+    },
+    errorText: {
+      type: String,
+      default: "",
+    },
+    editModeByDefault: Boolean,
+    blocked: Boolean,
   },
   data() {
     return {
       editmode: false,
+      emptyInput: this.errorText,
       currentSkill: {
         id: 0,
         title: this.skill.title,
@@ -49,7 +70,35 @@ export default {
   components: {
     icon,
     appInput: input,
+    tooltip: () => import("components/tooltip"),
   },
+
+   methods: {
+      onApprove() {
+
+        if (this.value === "") {
+          this.emptyInput = 'Заполните поле';
+        } else {
+          this.emptyInput = '';
+        }
+
+        if (this.title.trim() === "") return false;
+
+        if (this.title.trim() === this.value.trim()) {
+          this.editmode = false;
+        } else {
+          this.$emit("approve", this.value);
+        }
+      },
+      checkForEmptyInputValue (e) {
+        console.log(e);
+        if(e.length > 0) {
+          this.emptyInput = ''
+        } else {
+          this.emptyInput = 'Заполните поле';
+        }
+      }
+    },
 };
 </script>
 
