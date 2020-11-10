190c2a705dc6e572b329d04ccc2bc1c42111b4d3
Gregory Tomilin <gregory.tomilin@gmail.com>
2020-11-10 13:31:01 +0300

    Добавляем сообщение о пустой форме

--- src/admin/components/editLine/editLine.vue
+++ src/admin/components/editLine/editLine.vue
@@ -1,70 +1,91 @@
 <template>
-  <div class="edit-line-component" :class="{'blocked' : blocked}">
-    <div class="title" v-if="editmode === false">
-      <div class="text">{{value}}</div>
-      <div class="icon">
-        <icon symbol="pencil" grayscale @click="editmode = true"></icon>
-      </div>
-    </div>
-    <div v-else class="title">
-      <div class="input">
-        <app-input
-          placeholder="Название новой группы"
-          :value="value"
-          :errorText="errorText"
-          @input="$emit('input', $event)"
-          @keydown.native.enter="onApprove"
-          autofocus="autofocus"
-          no-side-paddings="no-side-paddings"
-        ></app-input>
-      </div>
-      <div class="buttons">
-        <div class="button-icon">
-          <icon symbol="tick" @click="onApprove"></icon>
+    <div class="edit-line-component" :class="{'blocked' : blocked}">
+        <div class="title" v-if="editmode === false">
+            <div class="text">{{value}}</div>
+            <div class="icon">
+                <icon symbol="pencil" grayscale @click="editmode = true"></icon>
+            </div>
         </div>
-        <div class="button-icon">
-          <icon symbol="cross" @click="$emit('remove')"></icon>
+        <div v-else class="title">
+            <div class="input">
+                <app-input
+                        placeholder="Название новой группы"
+                        :value="value"
+                        @input="checkForEmptyInputValue($event)"
+                        @keydown.native.enter="onApprove"
+                        autofocus="autofocus"
+                        no-side-paddings="no-side-paddings"
+                ></app-input>
+                <tooltip
+                        v-if="emptyMessage.length > 0"
+                        :errorText="errorText"
+                        :text="emptyMessage"
+                        slot="tooltip"
+                        v-model="emptyMessage"
+                ></tooltip>
+            </div>
+            <div class="buttons">
+                <div class="button-icon">
+                    <icon symbol="tick" @click="onApprove"></icon>
+                </div>
+                <div class="button-icon">
+                    <icon symbol="cross" @click="$emit('remove')"></icon>
+                </div>
+            </div>
         </div>
-      </div>
     </div>
-  </div>
 </template>
-
 <script>
-export default {
-  props: {
-    value: {
-      type: String,
-      default: "",
+  export default {
+    props: {
+      value: {
+        type: String,
+        default: "",
+      },
+      errorText: {
+        type: String,
+        default: "",
+      },
+      editModeByDefault: Boolean,
+      blocked: Boolean,
     },
-    errorText: {
-      type: String,
-      default: "",
+    data() {
+      return {
+        editmode: this.editModeByDefault,
+        title: this.value,
+        emptyMessage: this.errorText
+      };
     },
-    editModeByDefault: Boolean,
-    blocked: Boolean,
-  },
-  data() {
-    return {
-      editmode: this.editModeByDefault,
-      title: this.value,
-    };
-  },
-  methods: {
-    onApprove() {
-      if (this.title.trim() === "") return false;
-      if (this.title.trim() === this.value.trim()) {
-        this.editmode = false;
-      } else {
-        this.$emit("approve", this.value);
+    methods: {
+      onApprove() {
+
+        if (this.value === "") {
+          this.emptyMessage = 'Заполните поле';
+        } else {
+          this.emptyMessage = '';
+        }
+
+        if (this.title.trim() === "") return false;
+
+        if (this.title.trim() === this.value.trim()) {
+          this.editmode = false;
+        } else {
+          this.$emit("approve", this.value);
+        }
+      },
+      checkForEmptyInputValue (e) {
+        if(e.length > 0) {
+          this.emptyMessage = ''
+        } else {
+          this.emptyMessage = 'Заполните поле';
+        }
       }
     },
-  },
-  components: {
-    icon: () => import("components/icon"),
-    appInput: () => import("components/input"),
-  },
-};
+    components: {
+      icon: () => import("components/icon"),
+      appInput: () => import("components/input"),
+      tooltip: () => import("components/tooltip"),
+    },
+  };
 </script>
-
 <style lang="postcss" scoped src="./editLine.pcss"></style>
